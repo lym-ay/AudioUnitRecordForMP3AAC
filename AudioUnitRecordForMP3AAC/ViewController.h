e1e3c63d78984acc723b9faaef7534863b40a2eb
yanminli <lym_ay@sina.com>
2018-07-18 10:48:49 +0800

    Initial commit

---
+++ AudioUnitRecordForMP3AAC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioUnitRecordForMP3AAC
+//
+//  Created by olami on 2018/7/18.
+//  Copyright © 2018年 VIA Technologies, Inc. & OLAMI Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
